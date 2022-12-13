c1d5d31d2ce336ccfece3518e7007797f1f386e9
Nina Vergmann <nina152b@kea.stud.dk>
2022-12-13 12:39:28 +0100

    menuen er i gang og sitemap siden er næsten færdig

--- src/main.js
+++ src/main.js
@@ -1,3 +1,26 @@
 import "./style.css";
 /* Import af Motion One bibliotek */
-import { animate, stagger, inView, scroll, timeline } from "motion"
+import { animate, stagger, inView, scroll, timeline } from "motion";
+
+/* _________________________________burger menu til mobil__________________________________________ */
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+  console.log("sidenVises");
+  document
+    .querySelector("#menuknap_mobil")
+    .addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+  console.log("toggleMenu");
+  document.querySelector("#menu").classList.toggle("hidden");
+
+  let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+  if (erSkjult == true) {
+    document.querySelector("menuknap_mobil").textContent = "☰";
+  } else {
+    document.querySelector("menuknap_mobil").textContent = "X";
+  }
+}
