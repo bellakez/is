2d9c7af7c3d6c2369932ff696b83599dc03804c3
bellakez <bellakezia28@stud.kea.dk>
2022-12-14 17:29:45 +0100

    forside, footer, menu og gavekortsiden

--- src/main.js
+++ src/main.js
@@ -3,6 +3,21 @@ import "./style.css";
 import { animate, stagger, inView, scroll, timeline } from "motion";
 
 
+const acceptCookie = document.querySelector("#accepter");
+const denyCookie = document.querySelector("#afvis");
+const cookieboks = document.querySelector("#cookieboks");
+
+acceptCookie.addEventListener("click", fjernCookieBoks);
+denyCookie.addEventListener("click", fjernCookieBoks);
+
+function fjernCookieBoks() {
+  cookieboks.classList.add("hidden");
+  sessionStorage.setItem("fjerncookieboks", true);
+  //koden nedenunder, gør at boksen aldrig kommer igen, men ved at fjerne kommer den igen ved reload i browser
+  // }
+  // if (sessionStorage.getItem("fjerncookieboks")) {;
+  // cookieboks.classList.add("hidden");
+}
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 
@@ -15,4 +30,12 @@ function myFunction() {
   } else {
     x.style.display = "block";
   }
-} 
+}
+
+inView(".fotogalleryinview", ({ target }) => {
+  animate(
+    target.querySelectorAll("img"),
+    { x: [-2000, 0] },
+    { duration: 1, delay: stagger(1, { start: 0.25 }) }
+  );
+});
